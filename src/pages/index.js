86e3fe1b51c4f88076dacbbc0f8e91308cf41bf4
lpatmo <linda.peng@alumni.duke.edu>
2018-08-28 21:50:43 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Violin covers</h1>
+    <p>Check out these violin covers.</p>
+  </div>
+)
+
+export default IndexPage
